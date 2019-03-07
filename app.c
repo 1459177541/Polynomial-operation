95113a74cbde9b274c255e06c210e9ece45ddccf
1459177541 <1459177541@qq.com>
2019-03-07 23:01:22 +0800

    [update]解析多项式表达式

--- app.c
+++ app.c
@@ -2,6 +2,7 @@
  * GitHub：https://github.com/1459177541/Polynomial-operation
  **/
 #include<stdio.h>
+#include<malloc.h>
 typedef struct polynomial
 {
     double num;
@@ -13,27 +14,189 @@ typedef struct list
     struct list *next;
 }list, *pList;
 
-//加法
-pPolynomial addition(){
+//输入
+char* input(){
+    int size = 32;
+    int i = 0;
+    char *input = (char*)malloc(sizeof(char)*size);
+    char inChar;
+    while('\n' != (inChar = getchar())){
+        //扩容
+        if (i >= size) {
+            int newSize = size * 2;
+            char *newInput =  (char*)malloc(sizeof(char)*newSize);
+            for(int j = 0; j < i; j++)
+            {
+                newInput[j] = input[i];
+            }
+            free(input);
+            input = newInput;
+            size = newSize;
+        }        
+        input[i] = inChar;
+        i++;
+    }
+    input[i] = '\0';
+    return input;
+}
 
+//解析一个
+pPolynomial analysisTerm(char *in, int start, int end){
+    pPolynomial d = (pPolynomial)malloc(sizeof(polynomial));
+    int num = 0;
+    double decimal = 0;
+    double decimalMultiples = 1;
+    int flag = 0;
+    for(int i = start; i < end; i++)
+    {
+        //切换到x的指数
+        if ('x'==in[i]) {
+            d->num = num+decimal;
+            num = 0;
+            decimal = 0;
+            decimalMultiples = 1;
+            flag = 0;
+            continue;
+        }
+        //非法字符
+        if (('0'>in[i] || '9'<in[i]) && '.'!=in[i]) {
+            continue;
+        }
+        //切换到小数
+        if ('.' == in[i]) {
+            flag = 1;
+            continue;
+        }
+        //获得数值
+        if (flag) {
+            decimalMultiples/=10;
+            decimal = decimal + (in[i]-'0')*decimalMultiples;
+        }
+        else
+        {
+            num = num*10 + in[i]-'0';
+        }
+    }
+    d->power = (num == 0) ? 1 : (num+decimal);
+
+    if ('-'==in[start]) {
+        d->num *= -1;
+    }
+    for(int i = end; i > start; i--)
+    {
+        if ('x' == in[i]) {
+            break;
+        }
+        if ('-' == in[i]) {
+            d->power *= -1;
+            break;
+        }
+    }    
+    return d;
+}
+
+//解析表达式
+pList analysisPolynoials(char *in){
+    pList list = (pList)malloc(sizeof(pList));
+    list->data = NULL;
+    list->next = NULL;
+    pList p = list;
+
+    int i = 0;
+    int flag = 0;
+    int start = -1;
+    while('\0'!=in[i]){
+        if ('+' == in[i]) {
+            flag = 1;
+        }
+        if ('-' == in[i]) {
+            if (start+1==i) {
+                flag = 0;
+            }else{
+                int flag2 = 0;
+                for(int j = i; j > start; j--)
+                {
+                    if ('0' <= in[j] && '9'>= in[j]) {
+                        flag2 = 1;
+                    }
+                    if ('x' == in[j]) {
+                        if (flag2) {
+                            flag = 1;
+                        }
+                        break;
+                    }                    
+                }                
+            }
+        }        
+        if(flag){
+            start = (start < 0) ? 0 :start;
+            pList node = (pList)malloc(sizeof(list));
+            p->next = node;
+            node->data = analysisTerm(in, start, i);
+            node->next = NULL;
+            start = i;
+            p = p->next;
+            flag = 0;
+        }      
+        i++;
+    }
+    start = (start < 0) ? 0 :start;
+    pList node = (pList)malloc(sizeof(list));
+    p->next = node;
+    node->data = analysisTerm(in, start, i);
+    node->next = NULL;
+    
+    pList t = list->next;
+    list->next = NULL;
+    free(list);
+    return t;
+}
+
+//加法
+pList addition(){
+
+    
     return NULL;
 }
 
 //减法
-pPolynomial subtraction(){
+pList subtraction(){
 
     return NULL;
 }
 
 //乘法
-pPolynomial multiplication(){
+pList multiplication(){
 
     return NULL;
 }
 
 //输出
-void printPolynomial(pPolynomial polynomial){
-    
+void printPolynomial(pList polynomial){
+    if (NULL == polynomial) {
+        printf("0\n");
+        return;
+    }
+    int flag = 0;
+    while(NULL != polynomial){
+        pPolynomial data = (pPolynomial)polynomial->data;
+        if (!(data->num < 0) && flag) {
+            printf("+");
+        }
+        printf("%g", data->num);
+        if (data->num != 0) {
+            if (data->power == 1) {
+                printf("x");
+            }else if (data->power < 0) {
+                printf("x^(%g)", data->power);
+            }else if (data->power > 0) {
+                printf("x^%g", data->power);
+            }
+        }        
+        flag = 1;
+        polynomial = polynomial->next;
+    }
+    printf("\n");
 }
 
 void menu(){
@@ -45,7 +208,7 @@ void menu(){
     printf("请输入您的选择:");
     char in = getchar();
     while('\n'!=getchar()){}    
-    pPolynomial result;
+    pList result;
     switch (in)
     {
         case '1':
